cfd0027ae879db32095514e852de4b502e897099
qf <legend9308@163.com>
2017-05-03 21:08:53 +0800

    tanchuceng

---
+++ js/shop-cart.js
@@ -0,0 +1,11 @@
+var oTab=document.getElementById('tab'),
+	aTr=oTab.getElementsByTagName('tr');
+
+for(var i=1;i<aTr.length,i++){
+	aTr[i].onclick=function(){
+		var oCheck=this.getElementsByTagName('input')
+		if(indexOf(this.className,'selected')!=-1){
+			this.className=
+		}
+	}
+}
