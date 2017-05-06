94e0e352fbf53f83f779938caaeb77b62e240cb1
qf <legend9308@163.com>
2017-05-06 18:30:32 +0800

    shop-cart jq

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -1,11 +1,38 @@
-var oTab=document.getElementById('tab'),
-	aTr=oTab.getElementsByTagName('tr');
+// var oTab=document.getElementById('tab'),
+// 	oInp=oTab.getElementsByClassName('check');
 
-for(var i=1;i<aTr.length,i++){
-	aTr[i].onclick=function(){
-		var oCheck=this.getElementsByTagName('input')
-		if(indexOf(this.className,'selected')!=-1){
-			this.className=
-		}
+// oInp[0].onmouseup=function(){
+// 	if(this.checked=='true'||this.checked==''){
+// 		for (var i = 1; i < oInp.length; i++) {
+// 			oInp[i].checked='true';
+// 		}
+// 	}else if(this.checked=='false'){
+// 		for (var j = 1; j < oInp.length; j++) {
+// 			oInp[j].checked='false';
+// 		}
+// 	}
+// }
+
+$('#tab tr:not(.tbhead)').on('click',function(e){
+	if(e.target==$(this).find(':checkbox')[0]){
+		e.target.checked=!e.target.checked;
 	}
-}
+	if($(this).find(':checkbox').prop('checked')){
+		$(this).find(':checkbox').prop('checked',false).end().removeClass('selected');
+	}else{
+		$(this).find(':checkbox').prop('checked',true).end().addClass('selected');
+	}
+	if($('#tab tr:not(.tbhead)').length==$('#tab tr:not(.tbhead)').find(':checked').length){
+		$('#tab .tbhead').find(':checkbox').prop('checked',true);
+	}else{
+		$('#tab .tbhead').find(':checkbox').prop('checked',false);
+	}
+})
+$('.tbhead :checkbox').on('click',function(){
+	if(this.checked){
+		$('#tab tr:not(.tbhead)').find(':checkbox').prop('checked',true).end().addClass('selected');
+	}
+	else{
+		$('#tab tr:not(.tbhead)').find(':checkbox').prop('checked',false).end().removeClass('selected');
+	}
+})
