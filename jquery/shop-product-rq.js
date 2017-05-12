f7c17745a57ee5f024ea94503ffdd9da6206e1ec
qf <legend9308@163.com>
2017-05-12 12:31:36 +0800

    tanceng require more

--- jquery/shop-product-rq.js
+++ jquery/shop-product-rq.js
@@ -1,12 +1,18 @@
 requirejs.config({
-	baseUrl:'jquery',
-	paths:{
-		jquery:'jquery-1.12.4'
-	}
+    baseUrl: 'jquery/',
+    paths: {
+        jquery: 'jquery-1.12.4'
+    }
 });
-
 require(['jquery','dialog'],function($,dialog){
-	$('.small-img li').on('click',function(){
-		dialog.open();
-	});
-})
+    $('.small-img li').on('click',function(){
+        dialog.open({
+            width:500,
+            height:400,
+            opacity:0.9,
+            title:'haha',
+            content:'.div1'
+        });
+    });
+   // dialog.close();
+});
